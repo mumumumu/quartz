d146d3ac6b9d9f9a094c7cc17c956254359ae075
jeff <jeff@moo.dev>
2024-01-02 01:49:47 -0500

    Allow checkboxes to be enabled or disabled

--- quartz/plugins/transformers/gfm.ts
+++ quartz/plugins/transformers/gfm.ts
@@ -1,15 +1,20 @@
 import remarkGfm from "remark-gfm"
 import smartypants from "remark-smartypants"
+import { Root as HtmlRoot } from "hast"
 import { QuartzTransformerPlugin } from "../types"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import { PluggableList } from "unified"
+import { visit } from "unist-util-visit"
 
 export interface Options {
+  enableCheckboxes: boolean
   enableSmartyPants: boolean
   linkHeadings: boolean
 }
 
 const defaultOptions: Options = {
+  enableCheckboxes: true,
   enableSmartyPants: true,
   linkHeadings: true,
 }
@@ -24,28 +29,39 @@ export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | 
       return opts.enableSmartyPants ? [remarkGfm, smartypants] : [remarkGfm]
     },
     htmlPlugins() {
+      const plugins: PluggableList = []
+
       if (opts.linkHeadings) {
-        return [
-          rehypeSlug,
-          [
-            rehypeAutolinkHeadings,
-            {
-              behavior: "append",
-              properties: {
-                ariaHidden: true,
-                tabIndex: -1,
-                "data-no-popover": true,
-              },
-              content: {
-                type: "text",
-                value: " §",
-              },
+        plugins.push(rehypeSlug, [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "append",
+            properties: {
+              ariaHidden: true,
+              tabIndex: -1,
+              "data-no-popover": true,
             },
-          ],
-        ]
-      } else {
-        return []
+            content: {
+              type: "text",
+              value: " §",
+            },
+          },
+        ])
       }
+
+      if (opts.enableCheckboxes) {
+        plugins.push(() => {
+          return (tree: HtmlRoot) => {
+            visit(tree, "element", (node) => {
+              if (node.properties.type === "checkbox" && node.properties.disabled) {
+                node.properties.disabled = false
+              }
+            })
+          }
+        })
+      }
+
+      return plugins
     },
   }
 }
